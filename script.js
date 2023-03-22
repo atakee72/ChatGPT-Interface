0b1035b3b3792a02beaaa72e83e51fced84831c6
Ercan A <atakee@gmail.com>
2023-03-22 11:50:56 +0100

    Initial commit: the script file is working, one can have a chat in the console window.

---
+++ script.js
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+config();
+
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
+
+// console.log(process.env.API_KEY);
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+userInterface.prompt();
+userInterface.on("line", async (input) => {
+  const res = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  });
+  console.log(res.data.choices[0].message.content);
+  userInterface.prompt();
+});
